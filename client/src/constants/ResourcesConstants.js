5472092ecfee2de5cb930f65527e10d305fadab0
Roberto Aleksandrov <roberto.a@sbtech.com>
2018-01-10 09:00:18 +0200

    added resourcesReducer and ResourcesConstants

---
+++ client/src/constants/ResourcesConstants.js
@@ -0,0 +1,19 @@
+
+
+export const RESOURCES_ACTIONS = {
+    GET_ALL: {
+        FULFILLED: 'GET_ALL_FULFILLED',
+        REJECTED: 'GET_ALL_REJECTED',
+    },
+    GET_BY_ID: {
+        FULFILLED: 'GET_BY_ID_FULFILLED',
+        REJECTED: 'GET_BY_ID_REJECTED'
+    }
+};
+
+
+export const URLS = {
+    GET_ALL: '/resources',
+    GET_BY_ID: '/resources/{id}'
+};
+
