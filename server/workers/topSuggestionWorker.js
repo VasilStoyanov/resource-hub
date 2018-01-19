b5e00bef0b4f1fdb466e55795c8c24d26ae0508a
Roberto Aleksandrov <roberto.a@sbtech.com>
2018-01-19 12:19:02 +0200

    updated web workers.

--- server/workers/topSuggestionWorker.js
+++ server/workers/topSuggestionWorker.js
@@ -2,12 +2,6 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.6/Rx.js');
 
 const observable = Rx.Observable;
 
-const sortAlphabetic = (arr) => arr.sort((elOne, elTwo) => {
-    if (elOne.name < elTwo.name) return -1;
-    if (elOne.name > elTwo.name) return 1;
-    return 0;
-});  
-
 self.addEventListener('message', (e) => {
     const topCount = e.data.topCount;
     const userInput = e.data.userInput;
@@ -16,13 +10,6 @@ self.addEventListener('message', (e) => {
     Rx.Observable.from(data)
         .filter(item =>
             item.name.toLocaleLowerCase().indexOf(userInput.toLocaleLowerCase()) !== -1)
-        .reduce((acc, current) => {
-            acc.push(current);
-            return acc;
-        }, [])
-        .map(sortAlphabetic)
-        //.map(arr => arr.slice(topCount))
-        .flatMap(arr => Rx.Observable.from(arr))
         .take(topCount)
         .reduce((acc, current) => {
             acc.push(current);
