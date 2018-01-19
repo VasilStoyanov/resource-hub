d24655e8c9f8a69cc6619527ea0f77b1ca410322
VasilStoyanov <vasil.stoyanov94@gmail.com>
2018-01-19 16:33:25 +0200

    Gulp task for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
+const webpack = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+
+gulp.task('default', ['build']);
+
+gulp.task('build', () => gulp.src('./client/src/')
+  .pipe(webpack(webpackConfig))
+  .pipe(gulp.dest('./client/dist/')));
