0a4d7a7972a16690bf2edd51df5ec0d1f2021556
Vasil Stoyanov <vasil.stoyanov94@gmail.com>
2018-01-21 20:09:32 +0200

    Gulpfile default task now uses series

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
+const del = require('del');
 
-gulp.task('default', ['build']);
+gulp.task('clean', () => del(['./client/dist/*']));
 
 gulp.task('build', () => gulp.src('./client/src/')
   .pipe(webpack(webpackConfig))
   .pipe(gulp.dest('./client/dist/')));
+
+gulp.task('default', gulp.series('clean', 'build'));
