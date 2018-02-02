51a3b0562c4cb10f6717ce62a68e67e6b74be5dd
VasilStoyanov <vasil.stoyanov94@gmail.com>
2018-02-02 09:49:33 +0200

    Webpack devserver and hotwatch

--- server/app/app.js
+++ server/app/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const appConfig = require('./config');
 const routers = require('./routers');
 const auth = require('./auth');
+const path = require('path');
+
 
 const init = (data) => {
   const app = express();
@@ -9,6 +11,10 @@ const init = (data) => {
   appConfig.applyTo(app);
   auth.applyTo(app, data);
   routers.attachTo(app, data);
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
 
   return Promise.resolve(app);
 };
