d6e84416c3da9b12350b01f955d61d0421ff165f
VasilStoyanov <vasil.stoyanov94@gmail.com>
2018-02-27 11:53:25 +0200

    Added gulpfile for lint tasks, removed linting from webpack. Nodemon now watches only server

--- webpack.common.js
+++ webpack.common.js
@@ -23,30 +23,20 @@ module.exports = {
     publicPath: '/',
   },
   module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitError: true,
-          emitWarning: true,
-        },
-      }, {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }, {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }, {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-        },
-      }],
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    }, {
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+      },
+    }],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
